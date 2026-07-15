ff3d674bc81dd3fac2dea8e084c6ef5f07ef63fe
Sebastian Guerra Guerra <seguerrag@unal.edu.co>
2026-07-15 10:22:35 -0500

    lab5

--- homework/homework.py
+++ homework/homework.py
@@ -3,6 +3,8 @@ Escriba el codigo que ejecute la accion solicitada.
 """
 
 # pylint: disable=import-outside-toplevel
+import os
+import pandas as pd
 
 
 def clean_campaign_data():
@@ -49,8 +51,76 @@ def clean_campaign_data():
 
 
     """
+    carpeta = "files/input"
 
-    return
+    datos = []
+
+    for archivo in os.listdir(carpeta):
+        ruta = os.path.join(carpeta, archivo)
+
+        df = pd.read_csv(ruta)
+        datos.append(df)
+
+    df = pd.concat(datos, ignore_index = True)
+    print(df.columns.tolist())
+
+    
+    client = df[["client_id","age","job","marital","education","credit_default","mortgage"]].copy()
+
+    client["job"] = client["job"].str.replace(".", "").str.replace("-", "_")
+
+    client["education"] = client["education"].replace("unknown", pd.NA)
+
+    client["education"] = client["education"].str.replace(".", "_")
+
+    client["credit_default"] = client["credit_default"].apply(lambda x: 1 if x == "yes" else 0)
+
+    client["mortgage"] = client["mortgage"].apply(lambda x: 1 if x == "yes" else 0)
+
+
+
+
+    campaign = df[["client_id","number_contacts","contact_duration","previous_campaign_contacts","previous_outcome","campaign_outcome","day","month"]].copy()
+
+    campaign["previous_outcome"] = campaign["previous_outcome"].apply(lambda x: 1 if x == "success" else 0)
+    campaign["campaign_outcome"] = campaign["campaign_outcome"].apply(lambda x: 1 if x == "yes" else 0)
+    meses = {
+    "jan": "01",
+    "feb": "02",
+    "mar": "03",
+    "apr": "04",
+    "may": "05",
+    "jun": "06",
+    "jul": "07",
+    "aug": "08",
+    "sep": "09",
+    "oct": "10",
+    "nov": "11",
+    "dec": "12"}
+
+    campaign["month"] = campaign["month"].map(meses)
+    campaign["last_contact_date"] = ("2022-"+ campaign["month"]+ "-"+ campaign["day"].astype(str).str.zfill(2))
+    campaign = campaign.drop(columns=["day", "month"])
+
+
+    economics = df[["client_id","cons_price_idx","euribor_three_months"]].copy()
+    
+
+    
+    salida = "files/output"
+    os.makedirs(salida, exist_ok=True)
+
+    client.to_csv(f"{salida}/client.csv", index=False)
+    campaign.to_csv(f"{salida}/campaign.csv", index=False)
+    economics.to_csv(f"{salida}/economics.csv", index=False)
+
+
+
+
+    
+
+
+    return  client, campaign, economics
 
 
 if __name__ == "__main__":
